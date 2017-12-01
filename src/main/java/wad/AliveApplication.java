df5497b2be853c622527d907568eb956abcd204d
Kumara Saini M <skumara@lx7-fuxi124.ad.helsinki.fi>
2017-12-01 17:06:14 +0200

    travis integration, first try

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -11,7 +11,7 @@ public class AliveApplication {
     }
 
     public static String githubUrl() {
-        return "";
+        return "https://github.com/sainikumara/wad-alive.git";
     }
 
     public static String travisUrl() {
